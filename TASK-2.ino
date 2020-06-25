0497e8157a29b0220978d6a8a7e4536e9246ed35
Sarah-Z99 <67125437+Sarah-Z99@users.noreply.github.com>
2020-06-26 02:53:46 +0300

    Add files via upload

---
+++ TASK-2.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo sv;
+int value;
+void setup(){
+  Serial.begin(9600);
+  sv.attach(9);
+  
+}
+
+void loop(){
+  for(int i=0; i<180; i++) {
+    sv.write(i);
+    Serial.println(i);
+    delay(50);
+  }
+  for(int i=180; i>180; i--) {
+    sv.write(i);
+    Serial.println(i);
+    delay(50);
+  }
+}
